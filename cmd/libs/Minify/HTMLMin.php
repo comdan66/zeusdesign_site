749c1993d362587fb38a037e26ed1e465abcded0
OA Wu <comdan66@gmail.com>
2017-07-12 18:05:36 +0800

    no message

--- cmd/libs/Minify/HTMLMin.php
+++ cmd/libs/Minify/HTMLMin.php
@@ -8,6 +8,6 @@
 
 class HTMLMin {
   public static function minify ($html) {
-    return DEV ? $html : trim (preg_replace (array ('/\>[^\S ]+/su', '/[^\S ]+\</su', '/(\s)+/su'), array ('>', '<', '\\1'), $html));
+    return defined ('DEV') && DEV ? $html : trim (preg_replace (array ('/\>[^\S ]+/su', '/[^\S ]+\</su', '/(\s)+/su'), array ('>', '<', '\\1'), $html));
   }
 }
